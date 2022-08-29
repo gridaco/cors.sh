5d9390e4010d4df62e8bcd2671a78659c846221e
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-30 03:44:07 +0900

    .

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "CORS.SH | Docs",
+  title: "cors.sh",
   tagline: "Get started with CORS.SH",
   url: "https://docs.cors.sh",
   // @ts-ignore
