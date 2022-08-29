053647f65c32dd2842f51c5d1e797bbe0048e111
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-29 20:36:50 +0900

    update redirection

--- homepage/next.config.js
+++ homepage/next.config.js
@@ -2,26 +2,38 @@
  * @type {import('next').NextConfig}
  */
 const config = {
-  rewrites: [
-    {
-      source: "/:target",
-      destination: "https://api.cors.sh/:target",
-    },
-    {
-      source: "/playground",
-      destination: "https://playground.cors.sh",
-    },
-    {
-      source: "/playground/:path*",
-      destination: "https://playground.cors.sh/:path*",
-    },
-  ],
-  redirects: [
-    {
-      source: "/console",
-      destination: "https://console.grida.co/cors-proxy",
-      permanent: true,
-    },
-  ],
+  rewrites() {
+    return [
+      {
+        source: "/playground",
+        destination: "https://playground.cors.sh",
+      },
+      {
+        source: "/playground/:path*",
+        destination: "https://playground.cors.sh/:path*",
+      },
+    ];
+  },
+  redirects() {
+    return [
+      {
+        source: "/http\\:/:host/:path*",
+        destination: "https://cors.bridged.cc/http\\://:host/:path*",
+        basePath: false,
+        permanent: true,
+      },
+      {
+        source: "/https\\:/:host/:path*",
+        destination: "https://cors.bridged.cc/https\\://:host/:path*",
+        basePath: false,
+        permanent: true,
+      },
+      {
+        source: "/console",
+        destination: "https://console.grida.co/cors-proxy",
+        permanent: true,
+      },
+    ];
+  },
 };
 module.exports = config;
--- homepage/pages/_app.tsx
+++ homepage/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-XG051N1VS3"
         />
-        <script
+        <Script
           src={`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
