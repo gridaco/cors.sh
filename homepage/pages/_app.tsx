def9d11461315dc258a48eb60c9005fbd57c9e47
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-29 21:16:18 +0900

    update branding sources

--- homepage/pages/_app.tsx
+++ homepage/pages/_app.tsx
@@ -6,19 +6,22 @@ import Script from "next/script";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        {/* <!-- Google tag (gtag.js) --> */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-XG051N1VS3"
-        />
-        <Script
-          src={`window.dataLayer = window.dataLayer || [];
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-XG051N1VS3"
+      />
+      <Script
+        src={`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-XG051N1VS3');`}
-        />
+      />
+      <Head>
+        <title>CORS.SH</title>
+        <meta name="description" content="One CORS Proxy you'll ever need" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
