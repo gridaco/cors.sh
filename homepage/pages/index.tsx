44e3c03bfb6fcf8d3152f22d947206dc6e618c98
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-30 17:53:03 +0900

    grida add

--- homepage/pages/index.tsx
+++ homepage/pages/index.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { AppbarGroup } from "../grida/AppbarGroup";
 import { CorsSh } from "../grida/CorsSh";
+import { SectionCtaLast } from "../grida/SectionCtaLast";
+import { SectionDisclaimer } from "../grida/SectionDisclaimer";
+import { SectionHero } from "../grida/SectionHero";
+import { SectionPricing } from "../grida/SectionPricing";
 
 export default function HomePage() {
-  return <CorsSh />;
+  return (
+    <>
+      <AppbarGroup />
+      <SectionHero />
+      <SectionPricing />
+      <SectionCtaLast />
+      <SectionDisclaimer />
+    </>
+  );
 }
