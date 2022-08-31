f32cf5975cb0d05e5fb85f16fdcebc03b29cfc98
softmarshmallow <woojoo@softmarshmallow.com>
2022-09-01 06:46:30 +0900

    add og image

--- homepage/pages/_app.tsx
+++ homepage/pages/_app.tsx
@@ -40,6 +40,7 @@ export default function App({ Component, pageProps }: AppProps) {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <meta property="og:image" content="/og:image.jpg" />
       </Head>
       <ToastContainer />
       <Component {...pageProps} />
