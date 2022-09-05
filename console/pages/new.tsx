b5a1f34885a1356df14eaa35f5c8b812990eedbd
softmarshmallow <woojoo@softmarshmallow.com>
2022-09-05 23:40:01 +0900

    add create new app page

--- console/pages/new.tsx
+++ console/pages/new.tsx
@@ -1,4 +1,40 @@
 import React from "react";
+import { Button, TextFormField } from "@editor-ui/console";
 export default function NewApplicationPage() {
-  return <>create new application</>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        margin: "auto",
+        alignItems: "center",
+        justifyContent: "center",
+        maxWidth: "400px",
+        height: "100vh",
+      }}
+    >
+      <h1>Create new application</h1>
+      <div
+        style={{
+          marginTop: 60,
+          display: "flex",
+          flexDirection: "column",
+          gap: 21,
+          width: "100%",
+        }}
+      >
+        <TextFormField
+          label="Project Name"
+          placeholder="my-portfolio-website"
+        />
+        <TextFormField
+          label="You site"
+          placeholder="localhost:3000, www.my-site.com"
+          helpText="You can add up to 3 urls of your site"
+        />
+        <div style={{ height: 16 }} />
+        <Button height={"32px"}>Create Project</Button>
+      </div>
+    </div>
+  );
 }
