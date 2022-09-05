cdb24f041d9100da0ae4018d3ab88960656a6081
softmarshmallow <woojoo@softmarshmallow.com>
2022-09-06 08:29:32 +0900

    add font

--- homepage/pages/_document.tsx
+++ homepage/pages/_document.tsx
@@ -15,6 +15,11 @@ export default function _Document() {
         href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
         rel="stylesheet"
       />
+      {/* Nanum Pen Script */}
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap"
+        rel="stylesheet"
+      ></link>
       <Head />
 
       <body>
