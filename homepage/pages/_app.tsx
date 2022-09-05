3bc81ea15cb308deb77dc60c7b7478b6b99e0aee
softmarshmallow <woojoo@softmarshmallow.com>
2022-09-05 22:52:36 +0900

    update document

--- homepage/pages/_app.tsx
+++ homepage/pages/_app.tsx
@@ -23,23 +23,12 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>CORS.SH</title>
         <meta name="description" content="One CORS Proxy you'll ever need" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="One CORS Proxy you'll ever need" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="*"
-        />
-        {/* roboto mono */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        {/* inter */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
         <meta property="og:image" content="/og:image.jpg" />
       </Head>
 
---
+++ homepage/pages/_document.tsx
@@ -0,0 +1,26 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function _Document() {
+  return (
+    <Html lang="en">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="*" />
+      {/* roboto mono */}
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      {/* inter */}
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet"
+      />
+      <Head />
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
