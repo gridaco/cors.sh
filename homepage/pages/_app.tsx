d20382414522e0ac9b87ed85287f1ac03aeada61
softmarshmallow <woojoo@softmarshmallow.com>
2022-09-06 16:11:31 +0900

    update gtag

--- homepage/pages/_app.tsx
+++ homepage/pages/_app.tsx
@@ -10,18 +10,17 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       {/* <!-- Google tag (gtag.js) --> */}
       <Script
-        async
+        strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-XG051N1VS3"
       />
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-XG051N1VS3');`,
-        }}
-      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XG051N1VS3');
+        `}
+      </Script>
       <Head>
         <title>CORS.SH</title>
         <meta name="description" content="One CORS Proxy you'll ever need" />
