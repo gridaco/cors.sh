cf2bfe874c824dc5056f889cb8c92431b5297ab0
softmarshmallow <woojoo@softmarshmallow.com>
2022-09-06 21:14:26 +0900

    update state mng

--- homepage/pages/get-started/index.tsx
+++ homepage/pages/get-started/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import { PricingCardsList } from "../../layouts/pricing-card-list";
 import { StepLayout } from "../../layouts/step-layout";
 import * as k from "../../k";
@@ -6,41 +6,50 @@ import { useRouter } from "next/router";
 
 export default function GetstartedPage({ price: _price }: { price: string }) {
   const [price, setPrice] = React.useState(_price);
+  const stateRef = useRef<string>(_price);
+
+  stateRef.current = price;
 
   const router = useRouter();
+
+  const onPriceChange = (price: string) => {
+    console.log("price change", price);
+    setPrice(price);
+  };
+  console.log("price", price);
+
+  const onNextClick = useCallback(() => {
+    let redirect;
+    switch (stateRef.current) {
+      case k.PRICE_PAY_AS_YOU_GO: {
+        // TODO: add stripe integration.
+        redirect = "https://forms.gle/GXDGPAoM9fhZrQh77";
+        break;
+      }
+      case k.PRICE_PERSONAL_PRO_MONTHLY: {
+        redirect = k.SERVER_URL + "/payments/checkout-session?price=" + price;
+        break;
+      }
+      case k.PRICE_FREE_MONTHLY: {
+        // the free plan does not require payments, so we can skip to create new project right away.
+        redirect =
+          window.location.protocol + window.location.host + "/console/new";
+        break;
+      }
+    }
+
+    router.push("https://accounts.grida.co/signin?redirect_uri=" + redirect);
+  }, [price]);
 
   return (
     <StepLayout
       title="Get started"
       description="Ready to use cors.sh? select your plan and let’s create your first project."
-      onNextClick={() => {
-        let redirect;
-        switch (price) {
-          case k.PRICE_PAY_AS_YOU_GO: {
-            // TODO: add stripe integration.
-            redirect = "https://forms.gle/GXDGPAoM9fhZrQh77";
-            break;
-          }
-          case k.PRICE_PERSONAL_PRO_MONTHLY: {
-            redirect =
-              k.SERVER_URL + "/payments/checkout-session?price=" + price;
-            break;
-          }
-          case k.PRICE_FREE_MONTHLY: {
-            // the free plan does not require payments, so we can skip to create new project right away.
-            redirect = "/console/new";
-            break;
-          }
-        }
-
-        router.push(
-          "https://accounts.grida.co/signin?redirect_uri=" + redirect
-        );
-      }}
+      onNextClick={onNextClick}
       nextPromptLabel="Sign in with Grida and continue"
     >
       <PricingCardsList
-        onPriceChange={setPrice}
+        onPriceChange={onPriceChange}
         enableIndividualActions={false}
         // fallback to personal plan
         initialSelection={price}
