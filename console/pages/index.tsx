58cf8496da0f26883122296b3bf57de2357c4a8e
Universe <woojoo@softmarshmallow.com>
2023-01-03 11:37:59 +0900

    update console home applications list page

--- console/pages/index.tsx
+++ console/pages/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import styled from "@emotion/styled";
+import Link from "next/link";
+import { Logo } from "logo";
+import { FormPageLayout } from "layouts";
+import Head from "next/head";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { UnderlineButton } from "components";
 
 export default function ConsoleIndex({
   applications,
@@ -8,15 +14,44 @@ export default function ConsoleIndex({
 }) {
   return (
     <>
-      <h1>Applications</h1>
-      <div>
-        {applications.map((application) => (
-          <ApplicationItem key={application.id} {...application} />
-        ))}
-      </div>
+      <Head>
+        <title>CORS.SH - Console</title>
+      </Head>
+      <FormPageLayout>
+        <Logo />
+        <div style={{ height: 80 }} />
+        <ApplicationList>
+          {applications.map((application) => (
+            <ApplicationItem key={application.id} {...application} />
+          ))}
+        </ApplicationList>
+        <div style={{ height: 80 }} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 8,
+          }}
+        >
+          <UnderlineButton>
+            <Link href="/new">Create new application</Link>
+          </UnderlineButton>
+          <UnderlineButton>
+            <Link href="/subscriptions">Manage subscription</Link>
+          </UnderlineButton>
+        </div>
+      </FormPageLayout>
     </>
   );
 }
+
+const ApplicationList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  width: 100%;
+  gap: 8px;
+`;
 
 function ApplicationItem({
   id,
@@ -28,19 +63,37 @@ function ApplicationItem({
   allowedOrigins: string[];
 }) {
   return (
-    <ItemWrap href={`/console/${id}`}>
-      <div>
-        appid: {id}
-        <span>{name}</span>
-      </div>
-    </ItemWrap>
+    <Link href={`/${id}`}>
+      <ItemWrap>
+        <span>
+          {name} ({id})
+        </span>
+        <ArrowRightIcon />
+      </ItemWrap>
+    </Link>
   );
 }
 
-const ItemWrap = styled.a`
+const ItemWrap = styled.div`
+  flex: 1;
+  display: flex;
   cursor: pointer;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  flex: none;
   border-radius: 4px;
+  border: solid 1px rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
   padding: 21px;
+
+  &:hover {
+    border: solid 1px rgba(0, 0, 0, 0.1);
+    background-color: rgba(0, 0, 0, 0.02);
+    box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.1);
+  }
+
+  transition: all 0.2s ease-in-out;
 `;
 
 export async function getServerSideProps(context: any) {
@@ -49,8 +102,12 @@ export async function getServerSideProps(context: any) {
     props: {
       applications: [
         {
-          id: "123",
+          id: "1",
           name: "My app",
+        },
+        {
+          id: "2",
+          name: "My app 2",
         },
       ],
     },
