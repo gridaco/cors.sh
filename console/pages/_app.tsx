a114cc481b4423fa53b9f577d3f00c284463b5b9
Universe <woojoo@softmarshmallow.com>
2023-01-03 11:37:08 +0900

    add toaster to app root, add clipboard pkg

--- console/pages/_app.tsx
+++ console/pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Toaster position="bottom-center" />
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default App;
