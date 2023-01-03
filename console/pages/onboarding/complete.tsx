a334e61129711f21dd3e45e7bc58f961555faa4f
Universe <woojoo@softmarshmallow.com>
2023-01-03 09:53:06 +0900

    wip - onboarding complete page

---
+++ console/pages/onboarding/complete.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function InitialOnboardingFinalPage() {
+  return (
+    <>
+      <h1>Onboarding complete</h1>
+      <p>Now you can start using CORS.sh</p>
+    </>
+  );
+}
