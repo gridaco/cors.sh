016d44fdb927b9ef7362f23d3be6d9e5beeb23d3
softmarshmallow <woojoo@softmarshmallow.com>
2023-01-05 18:45:55 +0900

    update price

--- homepage/k/price.ts
+++ homepage/k/price.ts
@@ -3,12 +3,22 @@ const _PRICE_PERSONAL_PRO_MONTHLY = {
   live: "price_1LbnTwAvR3geCh5rZm9v8CAy",
 } as const;
 
+const _PRICE_PERSONAL_PRO_YEARLY = {
+  test: "price_1MMpznAvR3geCh5ro9O4Gdlt",
+  live: "price_1LbnV4AvR3geCh5rMEwJ5Zf1",
+};
+
 const _PRICE_FREE = "price_1KeF2VAvR3geCh5rq6s4V2P1";
 
 export const PRICE_PERSONAL_PRO_MONTHLY =
   process.env.NODE_ENV === "production"
     ? _PRICE_PERSONAL_PRO_MONTHLY.live
     : _PRICE_PERSONAL_PRO_MONTHLY.test;
+
+export const PRICE_PERSONAL_PRO_YEARLY =
+  process.env.NODE_ENV === "production"
+    ? _PRICE_PERSONAL_PRO_YEARLY.live
+    : _PRICE_PERSONAL_PRO_YEARLY.test;
 
 export const PRICE_PAY_AS_YOU_GO = "price_1LegsaAvR3geCh5rBCiuVmDt";
 
@@ -23,44 +33,65 @@ interface Price {
   features: string[];
 }
 
+const price_free: Price = {
+  id: PRICE_FREE_MONTHLY,
+  name: "For Testing",
+  price: "Free",
+  features: [
+    "10,000 requests per month",
+    "5GB traffic per month",
+    "100 requests per hour",
+    "2 Projects",
+    "3mb per request",
+  ],
+};
+
+const price_pro_monthly: Price = {
+  id: PRICE_PERSONAL_PRO_MONTHLY,
+  name: "For Personal projects",
+  price: "$4",
+  unit: "Month",
+  features: [
+    "Up to 500,000 requests per month",
+    "500GB Bandwidth",
+    "Unlimited Projects",
+    "No hourly request limit",
+    "Max 6mb per request",
+  ],
+};
+
+const price_pro_yearly: Price = {
+  id: PRICE_PERSONAL_PRO_YEARLY,
+  name: "Save 25% with annual billing",
+  price: "$36",
+  unit: "Year",
+  features: [
+    "Up to 500,000 requests per month",
+    "500GB Bandwidth",
+    "Unlimited Projects",
+    "No hourly request limit",
+    "Max 6mb per request",
+  ],
+};
+
+const price_pay_as_you_go: Price = {
+  id: PRICE_PAY_AS_YOU_GO,
+  name: "For Production projects",
+  description: "From $10 / month",
+  price: "Pay as you go",
+  features: [
+    "5,000,000 requests / month included",
+    "Unlimited Bandwidth",
+    "$1 per every next 100,000 requests",
+    "Unlimited Projects",
+    "No hourly request limit",
+    "Max 6mb per request",
+  ],
+};
+
 export const prices: Price[] = [
-  {
-    id: PRICE_FREE_MONTHLY,
-    name: "For Testing",
-    price: "Free",
-    features: [
-      "10,000 requests per month",
-      "5GB traffic per month",
-      "100 requests per hour",
-      "2 Projects",
-      "3mb per request",
-    ],
-  },
-  {
-    id: PRICE_PERSONAL_PRO_MONTHLY,
-    name: "For Personal projects",
-    price: "$4",
-    unit: "Month",
-    features: [
-      "Up to 500,000 requests per month",
-      "500GB Bandwidth",
-      "Unlimited Projects",
-      "No hourly request limit",
-      "Max 6mb per request",
-    ],
-  },
-  {
-    id: PRICE_PAY_AS_YOU_GO,
-    name: "For Production projects",
-    description: "From $10 / month",
-    price: "Pay as you go",
-    features: [
-      "5,000,000 requests / month included",
-      "Unlimited Bandwidth",
-      "$1 per every next 100,000 requests",
-      "Unlimited Projects",
-      "No hourly request limit",
-      "Max 6mb per request",
-    ],
-  },
+  // price_free,
+  price_pro_monthly,
+  price_pro_yearly,
+  // price_pay_as_you_go,
 ];
