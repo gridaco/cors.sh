3cdfe3cb3bb6749a78632516dd9d31bbd9ba9fe7
softmarshmallow <woojoo@softmarshmallow.com>
2023-01-08 01:26:16 +0900

    renew get started page

--- homepage/pages/get-started/index.tsx
+++ homepage/pages/get-started/index.tsx
@@ -1,24 +1,64 @@
-import React, { useCallback, useRef } from "react";
-import { PricingCardsList } from "../../layouts/pricing-card-list";
-import { StepLayout } from "../../layouts/step-layout";
-import * as k from "../../k";
+import React, { useCallback, useRef, useEffect } from "react";
 import { useRouter } from "next/router";
+import { FormPageLayout } from "@app/ui/layouts";
+import { validateUrls } from "@app/ui/utils";
+import {
+  Button,
+  FormFieldBase,
+  FormFieldLabel,
+  TextFormField,
+} from "@editor-ui/console";
+import Select from "react-select";
+import client from "@cors.sh/service-api";
+import * as k from "../../k";
 
 export default function GetstartedPage({ price: _price }: { price: string }) {
+  const [name, setName] = React.useState("");
   const [price, setPrice] = React.useState(_price);
+  const [allowedOrigins, setAllowedOrigins] = React.useState("");
+  const [valid, setValid] = React.useState(false);
+  const [isBusy, setIsBusy] = React.useState(false);
+  const [isValid, setIsValid] = React.useState(false);
   const stateRef = useRef<string>(_price);
 
   stateRef.current = price;
 
   const router = useRouter();
 
+  const pricing_options = Object.values(pricing);
+
   const onPriceChange = (price: string) => {
     console.log("price change", price);
     setPrice(price);
   };
-  console.log("price", price);
 
-  const onNextClick = useCallback(() => {
+  const onTypeKey = (key: string) => {
+    // todo: validate key via api
+    setValid(key.length > 10);
+  };
+
+  const onEnter = () => {
+    // this is required because onEnter can also be invoked from input's callback
+    if (valid) {
+      onNextClick();
+    }
+  };
+
+  useEffect(() => {
+    setIsValid(name.length > 0 && validateUrls(allowedOrigins));
+  }, [name, allowedOrigins]);
+
+  const onNextClick = useCallback(async () => {
+    // create onboarding application
+    const application = await client.onboardingWithForm({
+      name,
+      allowedOrigins: allowedOrigins.split(",").map((x) => x.trim()),
+      priceId: price,
+    });
+
+    const onboarding_id = application.id;
+
+    // redirect user to payment page
     let redirect;
     switch (stateRef.current) {
       case k.PRICE_PAY_AS_YOU_GO: {
@@ -33,7 +73,11 @@ export default function GetstartedPage({ price: _price }: { price: string }) {
         break;
       }
       default: {
-        redirect = k.SERVER_URL + "/payments/checkout-session?price=" + price;
+        let params = new URLSearchParams();
+        params.append("price", price);
+        params.append("onboarding", onboarding_id);
+        redirect = k.SERVER_URL + "/payments/checkout-session" + "?" + params;
+
         break;
       }
     }
@@ -42,21 +86,99 @@ export default function GetstartedPage({ price: _price }: { price: string }) {
   }, [price]);
 
   return (
-    <StepLayout
-      title="Get started"
-      description="Ready to use cors.sh? select your plan and let’s create your first project."
-      onNextClick={onNextClick}
-      nextPromptLabel="Sign in with Grida and continue"
-    >
-      <PricingCardsList
-        onPriceChange={onPriceChange}
-        enableIndividualActions={false}
-        // fallback to personal plan
-        initialSelection={price}
-      />
-    </StepLayout>
+    <FormPageLayout>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 8,
+        }}
+      >
+        <h1>Get started</h1>
+        <p>
+          Ready to use cors.sh? select your plan and let’s create your first
+          project. <br />
+          <p style={{ padding: 4, opacity: 0.5 }}>you can change this later</p>
+        </p>
+      </div>
+      <div className="form">
+        <TextFormField
+          label="Application name"
+          placeholder="my-portfolio-website"
+          onEnter={onEnter}
+          onChange={setName}
+        />
+        <TextFormField
+          label="Application origin URL"
+          placeholder="http://localhost:3000, https://my-site.com"
+          helpText="Add up to 3 urls of your site"
+          onEnter={onEnter}
+          onChange={setAllowedOrigins}
+        />
+        {/* pricing plan select */}
+
+        <FormFieldBase style={{ width: "100%" }}>
+          <FormFieldLabel>Plan</FormFieldLabel>
+          <div style={{ flex: 1, alignSelf: "stretch" }}>
+            <Select
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  onEnter();
+                }
+              }}
+              //
+              onChange={(e) => {
+                e && onPriceChange(e.value);
+              }}
+              value={pricing[price]}
+              options={pricing_options}
+            />
+          </div>
+        </FormFieldBase>
+        <div style={{ height: 16 }} />
+        <Button
+          disabled={!isValid || isBusy}
+          onClick={onNextClick}
+          height={"32px"}
+        >
+          Sign in with Grida and Continue
+        </Button>
+      </div>
+    </FormPageLayout>
+
+    // <StepLayout
+    //   title="Get started"
+    //   description="Ready to use cors.sh? select your plan and let’s create your first project."
+    //   onNextClick={onNextClick}
+    //   nextPromptLabel="Sign in with Grida and continue"
+    // >
+    //   <PricingCardsList
+    //     onPriceChange={onPriceChange}
+    //     initialSelection={price}
+    //   />
+    // </StepLayout>
   );
 }
+
+const pricing = {
+  [k.PRICE_PERSONAL_PRO_MONTHLY]: {
+    value: k.PRICE_PERSONAL_PRO_MONTHLY,
+    label: "Pro - $4/Mo",
+  },
+  [k.PRICE_PERSONAL_PRO_YEARLY]: {
+    value: k.PRICE_PERSONAL_PRO_YEARLY,
+    label: "Pro - $3/Mo (Pay annualy, save $12)",
+  },
+  [k.PRICE_ENTERPRISE_PRO_YEARLY]: {
+    value: k.PRICE_ENTERPRISE_PRO_YEARLY,
+    label: "Enterprise - $499/Yr",
+  },
+  // [k.PRICE_FREE_MONTHLY]: {
+  //   value: k.PRICE_FREE_MONTHLY,
+  //   label: "Free",
+  // },
+  // [k.PRICE_PAY_AS_YOU_GO]: {},
+} as const;
 
 export async function getServerSideProps(context: any) {
   const { price } = context.query;
