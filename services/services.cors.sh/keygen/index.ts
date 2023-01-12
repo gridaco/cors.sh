68258247c7c6e819009611534e22e9c07da21686
Universe <woojoo@softmarshmallow.com>
2023-01-12 18:06:07 +0900

    rm totp

--- services/services.cors.sh/keygen/index.ts
+++ services/services.cors.sh/keygen/index.ts
@@ -1,16 +1,17 @@
 import crypto from "crypto";
-import { totp } from "otplib";
 import day from "dayjs";
 
-const API_KEY_TEMP_AES_SECRET = process.env.API_KEY_TEMP_AES_SECRET;
-const API_KEY_TEMP_OTP_SECRET = process.env.API_KEY_TEMP_OTP_SECRET;
+const API_KEY_TEMP_AES_KEY = process.env.API_KEY_TEMP_AES_KEY;
+const API_KEY_TEMP_AES_IV: string = process.env.API_KEY_TEMP_AES_IV!;
+const _aes_key = Buffer.from(API_KEY_TEMP_AES_KEY, "hex");
+const _aes_iv = Buffer.from(API_KEY_TEMP_AES_IV, "hex");
+
 const API_KEY_TEST_HASH_SECRET = process.env.API_KEY_TEST_HASH_SECRET;
 const API_KEY_LIVE_HASH_SECRET = process.env.API_KEY_LIVE_HASH_SECRET;
 
 const API_KEY_HASH_SECRET_BY_TYPE = {
   test: API_KEY_TEST_HASH_SECRET,
   live: API_KEY_LIVE_HASH_SECRET,
-  temp: API_KEY_TEMP_OTP_SECRET,
 } as const;
 
 interface PermanentKey {
@@ -40,19 +41,14 @@ function live_key(signature: string): PermanentKey {
 export function sign_temporary_key() {
   const expires_at = day().add(TMP_KEY_EXP_IN_DAYS, "day");
 
-  totp.resetOptions();
-  totp.options = {
-    digits: 8,
-    epoch: expires_at.unix() * 1000,
-    step: 30,
-    window: 1,
-  };
-
-  const otp = totp.generate(API_KEY_TEMP_OTP_SECRET);
+  const token = expires_at.unix().toString();
 
   // convert to complex string with aes
-  let cipher = crypto.createCipher("aes-256-cbc", API_KEY_TEMP_AES_SECRET);
-  let encrypted = cipher.update(otp, "utf8", "hex") + cipher.final("hex");
+  let cipher = crypto.createCipheriv("aes-256-cbc", _aes_key, _aes_iv);
+
+  // this may be considered as insecure. yes. it may be vulnerable to ciphertext attack,
+  // even though it is fine because we have a rate-limit for temporary api keys and it does not have a clear usecase. for hackers to take benefits from this.
+  let encrypted = cipher.update(token, "utf8", "hex") + cipher.final("hex");
 
   return {
     key: prefix("temp") + "_" + encrypted,
