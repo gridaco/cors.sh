50c582100e24e1721b39e892feb7f3e3715ac5f5
softmarshmallow <woojoo@softmarshmallow.com>
2023-11-07 18:28:22 +0900

    add VignettingOverlay

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
 import { HomeCardExample } from '@/components/home-hover-card'
+import { SendMeAnApiKeyForm } from '@/components/landing/send-me-an-api-key-form'
 
 export default function Home() {
   return (
-    <main className="max-w-screen-xl overflow-x-hidden m-auto flex min-h-screen flex-col items-center md:items-stretch justify-center p-10 text-center md:text-start">
+    <main className="max-w-screen-xl overflow-x-visible m-auto flex min-h-screen flex-col items-center md:items-stretch justify-center p-10 text-center md:text-start">
       <div id='background' className='fixed -z-10 inset-0 select-none pointer-events-none'>
-        <Image src="/assets/background.svg" fill alt='background graphic' />
+        <div className='inset-0 flex items-center justify-center'>
+          <Image src="/assets/background.svg" layout="fill" objectFit="cover" alt='background graphic' />
+        </div>
+        <VignettingOverlay w={200} />
       </div>
+
       <div className='flex flex-col mb-40 mt-40 md:mb-0 md:mt-0 md:flex-row gap-20 justify-between items-center md:items-stretch'>
         <div className='flex flex-col gap-16 justify-center max-w-lg'>
           <div className='flex flex-col gap-5 max-w-lg'>
@@ -27,19 +32,39 @@ export default function Home() {
               Sick of cors errors? CORS.SH, A fast & reliable cors proxy for your websites here for the rescue.
             </p>
           </div>
-          <form className='flex flex-col md:flex-row gap-3'>
-            <input
-              className='p-5 text-neutral-100 bg-neutral-900 rounded-sm outline-none focus:outline-neutral-900 flex-1 min-w-[200px]'
-              placeholder='alice@acme.com' />
-            <button
-              className='p-5 pl-6 pr-6 border-neutral-900 border-2 rounded-sm text-xs lg:text-sm font-semibold bg-black'
-            >Send me an API Key</button>
-          </form>
+          <SendMeAnApiKeyForm />
         </div>
         <div className='flex'>
           <HomeCardExample />
         </div>
       </div>
     </main>
+  )
+}
+
+
+function VignettingOverlay({ w }: { w: number }) {
+  return (
+    <div className="absolute inset-0">
+      {/* Top edge */}
+      <div
+        style={{ height: w }}
+        className="absolute inset-x-0 top-0 bg-gradient-to-b from-black via-transparent to-transparent" />
+
+      {/* Bottom edge */}
+      <div
+        style={{ height: w }}
+        className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-black via-transparent to-transparent" />
+
+      {/* Left edge */}
+      <div
+        style={{ width: w }}
+        className="absolute inset-y-0 left-0 bg-gradient-to-r from-black via-transparent to-transparent" />
+
+      {/* Right edge */}
+      <div
+        style={{ width: w }}
+        className="absolute inset-y-0 right-0 bg-gradient-to-l from-black via-transparent to-transparent" />
+    </div>
   )
 }
