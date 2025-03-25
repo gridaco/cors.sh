86d6b28abe8dc8cd467f1c2c8afa2ae0efc378fd
Universe <universe@grida.co>
2025-03-25 23:29:48 +0900

    add blacklist

--- web/utils/email-validation.ts
+++ web/utils/email-validation.ts
@@ -1,7 +1,25 @@
+const blacklist = [
+  "nqmo.com",
+  "mailinator.com",
+  "10minutemail.com",
+  "guerrillamail.com",
+  "temp-mail.org",
+  "yopmail.com",
+  "throwawaymail.com",
+  "dispostable.com",
+  "getnada.com",
+  "maildrop.cc",
+];
+
+const pattern =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export const validateEmail = (email: string) => {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+  const emailLower = email.toLowerCase();
+  const domain = emailLower.split("@")[1];
+
+  if (!pattern.test(emailLower)) return false;
+  if (blacklist.includes(domain)) return false;
+
+  return true;
 };
